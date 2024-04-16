764c7a33a083ec15e479285eab242b0024af7a52
bretten <brettinbox@gmail.com>
2024-04-16 02:50:40 -0700

    #1 Adding filesystem entites

---
+++ src/FileSystem/DotSync.FileSystem.Domain/Files/Entities/File.cs
@@ -0,0 +1,16 @@
+﻿using com.brettnamba.DotSync.FileSystem.Domain.Files.ValueObjects;
+
+namespace com.brettnamba.DotSync.FileSystem.Domain.Files.Entities;
+
+public sealed class File
+{
+    public Guid Id { get; }
+
+    public Uri Path { get; }
+
+    public FileSha256Checksum Sha256Checksum { get; }
+
+    public DateTime UpdatedAt { get; }
+
+    public DateTime CreatedAt { get; }
+}
---
+++ src/FileSystem/DotSync.FileSystem.Domain/StorageLocations/Entities/StorageLocation.cs
@@ -0,0 +1,14 @@
+﻿using File = com.brettnamba.DotSync.FileSystem.Domain.Files.Entities.File;
+
+namespace com.brettnamba.DotSync.FileSystem.Domain.StorageLocations.Entities;
+
+public sealed class StorageLocation
+{
+    public Guid Id { get; }
+
+    public string Type { get; }
+
+    public bool IsSource { get; }
+
+    public IEnumerable<File> Files { get; }
+}
