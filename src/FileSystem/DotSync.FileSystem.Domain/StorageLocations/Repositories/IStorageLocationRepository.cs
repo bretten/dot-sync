d926a2e663005dd58f243fd98c9aabd03e74304e
bretten <brettinbox@gmail.com>
2024-04-21 14:55:39 -0700

    #1 StorageLocation

---
+++ src/FileSystem/DotSync.FileSystem.Domain/StorageLocations/Repositories/IStorageLocationRepository.cs
@@ -0,0 +1,9 @@
+﻿using com.brettnamba.DotSync.FileSystem.Domain.StorageLocations.Entities;
+
+namespace com.brettnamba.DotSync.FileSystem.Domain.StorageLocations.Repositories;
+
+public interface IStorageLocationRepository
+{
+    Task Add(StorageLocation storageLocation);
+    Task Update(StorageLocation storageLocation);
+}
