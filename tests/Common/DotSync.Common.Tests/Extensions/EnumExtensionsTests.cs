c12a244fb670a9d0562202ad773e96d3cd037ece
Brett <bretten@users.noreply.github.com>
2024-04-22 16:21:42 -0700

    #23 Enum display name extensions (#24)

---
+++ tests/Common/DotSync.Common.Tests/Extensions/EnumExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using com.brettnamba.DotSync.Common.Extensions;
+using com.brettnamba.DotSync.Common.Extensions.Exceptions;
+using com.brettnamba.DotSync.Common.Tests.Extensions.TestClasses;
+
+namespace com.brettnamba.DotSync.Common.Tests.Extensions;
+
+public class EnumExtensionsTests
+{
+    [Fact]
+    public void GetDisplayName_EnumWithoutDisplayAttribute_ThrowsException()
+    {
+        // Arrange
+        var enumMember = TestGetDisplayNameEnum.EnumWithoutDisplayAttribute;
+        var action = () => enumMember.GetDisplayName();
+
+        // Act
+        var actual = Record.Exception(action);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.IsType<EnumDisplayNameNotFoundException>(actual);
+    }
+
+    [Fact]
+    public void GetDisplayName_EnumWithDisplayAttribute_ThrowsException()
+    {
+        // Arrange
+        var enumMember = TestGetDisplayNameEnum.EnumWithDisplayAttribute;
+        var action = () => enumMember.GetDisplayName();
+
+        // Act
+        var actual = Record.Exception(action);
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.IsType<UndefinedEnumDisplayNameException>(actual);
+    }
+
+    [Fact]
+    public void GetDisplayName_EnumWithDisplayAttributeAndName_ReturnsName()
+    {
+        // Arrange
+        var enumMember = TestGetDisplayNameEnum.EnumWithDisplayAttributeAndName;
+
+        // Act
+        var actual = enumMember.GetDisplayName();
+
+        // Assert
+        Assert.Equal("Has DisplayAttribute and name", actual);
+    }
+}
