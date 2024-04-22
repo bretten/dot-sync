b401b89d4531377c7691d7684075e0adc04e1411
bretten <brettinbox@gmail.com>
2024-04-22 16:21:31 -0700

    #23 Enum display name extensions

---
+++ src/Common/DotSync.Common/Extensions/Exceptions/EnumDisplayNameNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace com.brettnamba.DotSync.Common.Extensions.Exceptions;
+
+/// <summary>
+/// Thrown when <see cref="EnumExtensions"/> cannot find a <see cref="DisplayAttribute"/>
+/// on an enum member
+/// </summary>
+public sealed class EnumDisplayNameNotFoundException : Exception
+{
+    public EnumDisplayNameNotFoundException(string? message) : base(message)
+    {
+    }
+}
