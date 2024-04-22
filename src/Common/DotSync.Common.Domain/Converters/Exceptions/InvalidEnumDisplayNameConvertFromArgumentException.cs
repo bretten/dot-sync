e97b7a4deb2b3df60e4e20584a1ddb8e63906898
bretten <brettinbox@gmail.com>
2024-04-22 15:56:32 -0700

    #21 Enum display name converter

---
+++ src/Common/DotSync.Common.Domain/Converters/Exceptions/InvalidEnumDisplayNameConvertFromArgumentException.cs
@@ -0,0 +1,11 @@
+﻿namespace DotSync.Common.Domain.Converters.Exceptions;
+
+/// <summary>
+/// Thrown when <see cref="EnumDisplayNameConverter"/> is provided an unsupported parameter in ConvertFrom
+/// </summary>
+public class InvalidEnumDisplayNameConvertFromArgumentException : Exception
+{
+    public InvalidEnumDisplayNameConvertFromArgumentException(string? message) : base(message)
+    {
+    }
+}
