3aaafa679b95b0e10464687bfae9748778d4eeba
bretten <brettinbox@gmail.com>
2024-04-22 01:03:17 -0700

    #5 Added FileSystem Infrastructure and ConsoleApp projects

---
+++ src/FileSystem/Apps/DotSync.FileSystem.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using com.brettnamba.DotSync.FileSystem.Application.Console;
+using com.brettnamba.DotSync.FileSystem.Application.Orchestration;
+using com.brettnamba.DotSync.FileSystem.Application.Reporting;
+using com.brettnamba.DotSync.FileSystem.Domain.FileIntegrity.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+//builder.Services.AddTransient<IFileRepository>();
+//builder.Services.AddTransient<IStorageLocationRepository>();
+//builder.Services.AddTransient<IFileChecksumGenerator, >();
+builder.Services.AddTransient<IFileIntegrityVerifier, LocalFileSystemFileIntegrityVerifier>();
+
+builder.Services.AddTransient<IDirectoryVerificationService, DirectoryVerificationService>();
+builder.Services.AddTransient<IIntegrityReporter, HtmlIntegrityReporter>();
+builder.Services.AddTransient<IConsoleHandler, ConsoleHandler>();
+
+using var host = builder.Build();
+
+
+await host.RunAsync();
