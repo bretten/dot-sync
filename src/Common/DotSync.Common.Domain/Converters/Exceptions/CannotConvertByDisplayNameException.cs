8f579ce25efae30c2004fdaac5333d7b570b75c0
Brett <bretten@users.noreply.github.com>
2024-04-22 15:57:02 -0700

    #21 Enum display name converter (#22)

---
+++ src/Common/DotSync.Common.Domain/Converters/Exceptions/CannotConvertByDisplayNameException.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotSync.Common.Domain.Converters.Exceptions;
+
+/// <summary>
+/// Thrown when <see cref="EnumDisplayNameConverter"/> cannot find a string value that corresponds to an enum member's
+/// <see cref="DisplayAttribute"/> name
+/// </summary>
+public sealed class CannotConvertByDisplayNameException : Exception
+{
+    public CannotConvertByDisplayNameException(string? message) : base(message)
+    {
+    }
+}
