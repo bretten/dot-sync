0c6e58e78b4113b27847233b03406209a4f2e6e3
bretten <brettinbox@gmail.com>
2024-04-23 15:53:43 -0700

    #7 Sha256 implementation of IFileChecksumGenerator

---
+++ src/FileSystem/DotSync.FileSystem.Infrastructure/FileIntegrity/Services/Sha256FileChecksumGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+using com.brettnamba.DotSync.FileSystem.Domain.FileIntegrity.Services;
+
+namespace com.brettnamba.DotSync.FileSystem.Infrastructure.FileIntegrity.Services;
+
+/// <summary>
+/// Generates a SHA256 file checksum
+/// </summary>
+public sealed class Sha256FileChecksumGenerator : IFileChecksumGenerator
+{
+    /// <summary>
+    /// <inheritdoc cref="IFileChecksumGenerator"/>
+    /// </summary>
+    public string GenerateChecksum(FileInfo fileInfo)
+    {
+        using SHA256 sha256 = SHA256.Create();
+        using FileStream fileStream = fileInfo.Open(FileMode.Open);
+
+        // Beginning of the file stream
+        fileStream.Position = 0;
+
+        byte[] hashValue = sha256.ComputeHash(fileStream);
+
+        return Convert.ToBase64String(hashValue, 0, hashValue.Length);
+    }
+}
---
+++ tests/FileSystem/DotSync.FileSystem.Infrastructure.Tests/FileIntegrity/Services/Sha256FileChecksumGeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using com.brettnamba.DotSync.FileSystem.Infrastructure.FileIntegrity.Services;
+
+namespace com.brettnamba.DotSync.FileSystem.Infrastructure.Tests.FileIntegrity.Services;
+
+public class Sha256FileChecksumGeneratorTests
+{
+    private const string TestFilesPath = "FileIntegrity/Services/TestFiles/";
+
+    [Fact]
+    public void GenerateChecksum_File_ReturnsChecksum()
+    {
+        // Arrange
+        var fileInfo = new FileInfo($"{TestFilesPath}checksum_file.txt");
+        var generator = new Sha256FileChecksumGenerator();
+
+        // Act
+        var actual = generator.GenerateChecksum(fileInfo);
+
+        // Assert
+        Assert.Equal("gc8+ZmyAj0xclM2CVqRQdCDaFRyRXeyxncPGnZqlERM=", actual);
+    }
+}
