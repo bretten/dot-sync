213601814b41c27e1cd4a825c15954d82993157d
bretten <brettinbox@gmail.com>
2026-01-09 04:09:50 -0800

    #147 Form submit and error messages moved to components

--- src/FileSystem/Apps/DotSync.FileSystem.WebApp/Components/Jobs/Forms/BaseForm.cs
+++ src/FileSystem/Apps/DotSync.FileSystem.WebApp/Components/Jobs/Forms/BaseForm.cs
@@ -7,9 +7,22 @@ public abstract class BaseForm : ComponentBase
 {
     [CascadingParameter] protected IMudDialogInstance? MudDialog { get; set; }
 
+    private bool _firstRenderDone = false;
     protected MudForm Form = null!;
     protected bool IsValid;
     protected string[] Errors = [];
+
+    protected override Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!_firstRenderDone)
+        {
+            // This allows checking if the markup has only been rendered once
+            // The param firstRender does not take into account needing to set event handler on a callback (IsSubmitButtonDisabled)
+            _firstRenderDone = true;
+        }
+
+        return base.OnAfterRenderAsync(firstRender);
+    }
 
     protected virtual Task Submit()
     {
@@ -19,5 +32,10 @@ public abstract class BaseForm : ComponentBase
         }
 
         return Task.CompletedTask;
+    }
+
+    protected bool IsSubmitButtonDisabled()
+    {
+        return _firstRenderDone && !IsValid;
     }
 }
