ee974f4bd6177dd6c47c84aeceba5ada705d21e9
bretten <brettinbox@gmail.com>
2026-01-22 01:41:14 -0800

    #196 Removed Blazor QuickGrid

--- src/FileSystem/Apps/DotSync.FileSystem.WebApp/Components/Pages/Home.razor.js
+++
@@ -1,9 +0,0 @@
-export const uploadButton = document.getElementById('upload-btn')
-export const uploadAlert = document.getElementById('upload-alert')
-
-if (uploadButton) {
-    const toast = bootstrap.Toast.getOrCreateInstance(uploadAlert)
-    uploadButton.addEventListener('click', () => {
-        toast.show()
-    })
-}
