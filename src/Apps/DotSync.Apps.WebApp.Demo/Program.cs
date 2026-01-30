766dd6b26bba5321ceb707965771726a5a6ffb59
bretten <brettinbox@gmail.com>
2026-01-29 16:59:50 -0800

    #232 Job details dialog is now launched after a new job

--- src/Apps/DotSync.Apps.WebApp.Demo/Program.cs
+++ src/Apps/DotSync.Apps.WebApp.Demo/Program.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using com.brettnamba.DotSync.Apps.Common.Auth;
+using com.brettnamba.DotSync.Apps.Common.Components.Jobs;
 using com.brettnamba.DotSync.Apps.Common.Components.Pages;
 using com.brettnamba.DotSync.Apps.Common.Startup;
 using com.brettnamba.DotSync.Common.DateAndTme;
@@ -73,6 +74,7 @@ builder.Services.AddJobs(builder.Configuration, new List<Assembly>()
     typeof(VerifyJobRunner).Assembly
 });
 builder.Logging.AddJobLogging();
+builder.Services.AddScoped<JobDetailsProvider>();
 
 // File systems
 await builder.Services.AddFileSystems(builder.Configuration);
